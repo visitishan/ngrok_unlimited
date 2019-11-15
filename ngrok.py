63669719b650bf221cc639431c795662732d4abe
Ishan Jain <visitishan@gmail.com>
2019-11-15 23:49:44 +0530

    fixed the code for restarting in 7 hours

--- ngrok.py
+++ ngrok.py
@@ -3,20 +3,30 @@ import requests, time, json, os, threading
 def temp():
     os.system('cmd /k "cd C://Users//AMIT//downloads//ngrok && ngrok  http 5000"')
 
-try:
-   x = threading.Thread(target = temp)
-   x.start()
-except:
-   print("Error: unable to start thread")
+while True:
+    try:
+       x = threading.Thread(target = temp)
+       x.start()
+    except:
+       print("Error: unable to start thread")
 
-time.sleep(5)
+    time.sleep(5)
 
-s = requests.Session()
-a = s.get("http://127.0.0.1:4040/api/tunnels").text
-b = json.loads(a)
-c= b['tunnels'][0]['public_url']
-print(c)
+    # s = requests.Session()
+    a = requests.get("http://127.0.0.1:4040/api/tunnels").text
+    b = json.loads(a)
+    c= b['tunnels'][0]['public_url']
+    print(c)
 
-
-link = {"link":c}
-d = s.get('http://yourdomainname.com/index.php',body = link)  #Update the server for tunnel link
+    headers = {
+    "User-Agent": "PostmanRuntime/7.19.0",
+    }
+    link = {"link":c}
+    url = <link of your php script>
+    d = requests.get(url = url, params = link, headers = headers)  #Update the server for tunnel link
+    print('request data ---- ', d.text)
+    full_time = 3600 * 7
+    time.sleep(full_time)
+    os.system("taskkill /f /im ngrok.exe")
+    time.sleep(10)
+    
